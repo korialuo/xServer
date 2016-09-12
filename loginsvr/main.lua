db64af7367f5e68ae5e4698f0dfd00a52f07dd1a
korialuo <koria.luo@foxmail.com>
2016-09-12 15:02:49 +0800

    fix loginsvr

--- loginsvr/main.lua
+++ loginsvr/main.lua
@@ -2,13 +2,14 @@ local skynet = require "skynet"
 
 skynet.start(function()
     local conf = {
-        address = tostring(skynet.getenv("address")),
-        port = tonumber(skynet.getenv("port")),
-        maxclient = tonumber(skynet.getenv("maxclient")),
-        nodelay = true
+        address = assert(tostring(skynet.getenv("address"))),
+        port = assert(tonumber(skynet.getenv("port"))),
+        maxclient = assert(tonumber(skynet.getenv("maxclient"))),
+        nodelay = not not (skynet.getenv("nodelay") == "true")
     }
+    local instance = tonumber(skynet.getenv("instance"))
     skynet.call(
-        skynet.newservice("gatesvr", skynet.newservice("loginsvr", "master", 2)),
+        skynet.newservice("gatesvr", skynet.newservice("loginsvr", "master", instance)),
         "lua",
         "open",
         conf
